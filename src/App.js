304f3080dee8654c91cf7738a8d2fb91128c3190
xingjian <deric.pan@gmail.com>
2017-02-22 10:36:33 +0800

    add wilddog

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { View, Text } from 'react-native';
+import * as wilddog from 'wilddog';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      authDomain: 'skyline99.wilddog.com',
+    };
+    wilddog.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
