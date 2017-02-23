622721a6c304a79e93f8f89fcf251a5a52aad174
xingjian <deric.pan@gmail.com>
2017-02-23 15:52:02 +0800

    employee form done

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
 import { employeeUpdate } from '../actions';
@@ -25,18 +26,39 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={this.props.shift}
+            onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day })}
+          >
+            <Picker.Item lable="Monday" value="Monday" />
+            <Picker.Item lable="Tuesday" value="Tuesday" />
+            <Picker.Item lable="Wednesday" value="Wednesday" />
+            <Picker.Item lable="Thursday" value="Thursday" />
+            <Picker.Item lable="Friday" value="Friday" />
+            <Picker.Item lable="Saturday" value="Saturday" />
+            <Picker.Item lable="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+
         <CardSection>
           <Button>
             Create
           </Button>
         </CardSection>
-
-        <CardSection>
-        </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+};
 
 const mapStateToPros = (state) => {
   const { name, phone, shift } = state.employeeForm;
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { View } from 'react-native';
 
 const CardSection = (props) => {
-  return(
-    <View style={styles.containerStyle}>
+  return (
+    <View style={[styles.containerStyle, props.style]}>
       {props.children}
     </View>
   );
